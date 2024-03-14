6c5d8375faa4de74803ff9e865e61cea79506160
Andrei M <98398587+andrei-wm@users.noreply.github.com>
2024-03-14 12:36:35 +0200

    small fix

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAoTTdAXY0KPcvWBYouvLLZjzB4SCD328M",
+  authDomain: "crwn-clothing-db-fea18.firebaseapp.com",
+  projectId: "crwn-clothing-db-fea18",
+  storageBucket: "crwn-clothing-db-fea18.appspot.com",
+  messagingSenderId: "84316300100",
+  appId: "1:84316300100:web:e37f25a065663a5d71ed11"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
